3c34a2e941556c9a006dbec6fa7efd3192b0b975
nfink <fink.nolan@gmail.com>
2015-07-27 15:28:37 +0900

    React.MSBuild doesn't play nicely with Azure publish, so wrote a custom jsxtransform based on it.

--- Haven/HavenWebApp/MenuModule.cs
+++ Haven/HavenWebApp/MenuModule.cs
@@ -11,6 +11,8 @@ using Newtonsoft.Json;
 using Nancy.Conventions;
 using Nancy.Security;
 using Nancy.Authentication.Forms;
+using React;
+using System.IO;
 
 namespace HavenWebApp
 {
@@ -18,12 +20,33 @@ namespace HavenWebApp
     {
         public static DataLoad DataLoad;
 
-        public MenuModule()
+        public MenuModule(IRootPathProvider pathProvider)
         {
             if (DataLoad == null)
             {
                 DataLoad = new DataLoad();
                 DataLoad.LoadTables();
+            }
+
+            TransformJSX(pathProvider);
+        }
+
+        private static void TransformJSX(IRootPathProvider pathProvider)
+        {
+            bool useHarmony = false;
+            bool stripTypes = false;
+            var config = React.AssemblyRegistration.Container.Resolve<IReactSiteConfiguration>();
+            config
+                .SetReuseJavaScriptEngines(false)
+                .SetUseHarmony(useHarmony)
+                .SetStripTypes(stripTypes);
+
+            var environment = React.AssemblyRegistration.Container.Resolve<IReactEnvironment>();
+            string root = pathProvider.GetRootPath();
+            var files = Directory.EnumerateFiles(root + "Scripts", "*.jsx", SearchOption.AllDirectories);
+            foreach (var path in files)
+            {
+                environment.JsxTransformer.TransformAndSaveJsxFile(path.Replace(root, string.Empty));
             }
         }
     }
